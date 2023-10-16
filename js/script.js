09c59dce1f01b906cabbdb5af404c0ac32592176
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-10-16 12:20:27 -0400

    add table through js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
-console.log("connected");
+// Part 1: Dynamic site visitor data
+
+const visitors = [
+  { month: "April", visits: 1221 },
+  { month: "May", visits: 2221 },
+  { month: "June", visits: 2376 },
+  { month: "July", visits: 6551 },
+  { month: "August", visits: 8776 },
+  { month: "September", visits: 9976 },
+];
+
+const visitorsSection = document.querySelector("#visitors-section");
+
+const table = document.createElement("table");
+const tableRow = document.createElement("tr");
+table.appendChild(tableRow);
+
+const tableHeadMonth = document.createElement("th");
+tableHeadMonth.appendChild(document.createTextNode("Month"));
+const tableHeadVisits = document.createElement("th");
+tableHeadVisits.appendChild(document.createTextNode("Visits"));
+
+tableRow.appendChild(tableHeadMonth);
+tableRow.appendChild(tableHeadVisits);
+
+table.setAttribute("id", "visitors-table");
+visitorsSection.appendChild(table);
+
+for (let i = 0; i < visitors.length; i++) {
+  const newTableRow = document.createElement("tr");
+  const tableDataMonth = document.createElement("td");
+
+  tableDataMonth.appendChild(document.createTextNode(visitors[i].month));
+  const tableDataVisits = document.createElement("td");
+  tableDataVisits.appendChild(document.createTextNode(visitors[i].visits));
+
+  newTableRow.appendChild(tableDataMonth);
+  newTableRow.appendChild(tableDataVisits);
+  table.appendChild(newTableRow);
+}
+
+visitorsSection.appendChild(table);
+
+// Part 2: GDPR
 
 const cookies = document.querySelector("#cookies-banner");
 
