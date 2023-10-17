371d5dca50bfe8c7450a322b300846c434eae4e9
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-10-16 21:41:19 -0400

    add cookies and contrast button to all files

--- js/script.js
+++ js/script.js
@@ -11,67 +11,71 @@ const visitors = [
 
 const visitorsSection = document.querySelector("#visitors-section");
 
-const table = document.createElement("table");
-const tableRow = document.createElement("tr");
-table.appendChild(tableRow);
+if (visitorsSection) {
+  const table = document.createElement("table");
+  const tableRow = document.createElement("tr");
+  table.appendChild(tableRow);
 
-const tableHeadMonth = document.createElement("th");
-tableHeadMonth.appendChild(document.createTextNode("Month"));
-const tableHeadVisits = document.createElement("th");
-tableHeadVisits.appendChild(document.createTextNode("Visits"));
+  const tableHeadMonth = document.createElement("th");
+  tableHeadMonth.appendChild(document.createTextNode("Month"));
+  const tableHeadVisits = document.createElement("th");
+  tableHeadVisits.appendChild(document.createTextNode("Visits"));
 
-tableRow.appendChild(tableHeadMonth);
-tableRow.appendChild(tableHeadVisits);
+  tableRow.appendChild(tableHeadMonth);
+  tableRow.appendChild(tableHeadVisits);
 
-table.setAttribute("id", "visitors-table");
-visitorsSection.appendChild(table);
+  table.setAttribute("id", "visitors-table");
 
-for (let i = 0; i < visitors.length; i++) {
-  const newTableRow = document.createElement("tr");
-  const tableDataMonth = document.createElement("td");
+  visitorsSection.appendChild(table);
 
-  tableDataMonth.appendChild(document.createTextNode(visitors[i].month));
-  const tableDataVisits = document.createElement("td");
-  tableDataVisits.appendChild(document.createTextNode(visitors[i].visits));
+  for (let i = 0; i < visitors.length; i++) {
+    const newTableRow = document.createElement("tr");
+    const tableDataMonth = document.createElement("td");
 
-  newTableRow.appendChild(tableDataMonth);
-  newTableRow.appendChild(tableDataVisits);
-  table.appendChild(newTableRow);
+    tableDataMonth.appendChild(document.createTextNode(visitors[i].month));
+    const tableDataVisits = document.createElement("td");
+    tableDataVisits.appendChild(document.createTextNode(visitors[i].visits));
+
+    newTableRow.appendChild(tableDataMonth);
+    newTableRow.appendChild(tableDataVisits);
+    table.appendChild(newTableRow);
+  }
+
+  visitorsSection.appendChild(table);
 }
-
-visitorsSection.appendChild(table);
 
 // Part 2: GDPR
 
 const cookies = document.querySelector("#cookies-banner");
+if (cookies) {
+  cookies.children[0].addEventListener("click", acceptOrRevokeCookies);
 
-cookies.children[0].addEventListener("click", acceptOrRevokeCookies);
-
-function acceptOrRevokeCookies(e) {
-  if (e.target.id === "accept-cookies") {
-    e.target.remove();
-    const p = document.createElement("p");
-    const a = document.createElement("a");
-    const newBannerContent = document.createTextNode(
-      "Cookies were accepted. Would you like to "
-    );
-    const revokeText = document.createTextNode("Revoke?");
-    p.appendChild(newBannerContent);
-    a.appendChild(revokeText);
-    a.setAttribute("href", "#");
-    a.setAttribute("id", "revoke-cookies");
-    a.addEventListener("click", acceptOrRevokeCookies);
-    p.appendChild(a);
-    cookies.append(p);
-  } else {
-    e.target.parentElement.remove();
-    const a = document.createElement("a");
-    const cookiesText = document.createTextNode("Accept cookies");
-    a.appendChild(cookiesText);
-    a.setAttribute("href", "#");
-    a.setAttribute("id", "accept-cookies");
-    a.addEventListener("click", acceptOrRevokeCookies);
-    cookies.appendChild(a);
+  function acceptOrRevokeCookies(e) {
+    if (e.target.id === "accept-cookies") {
+      e.target.remove();
+      const p = document.createElement("p");
+      const a = document.createElement("a");
+      const newBannerContent = document.createTextNode(
+        "Cookies were accepted. Would you like to "
+      );
+      const revokeText = document.createTextNode("Revoke?");
+      p.appendChild(newBannerContent);
+      a.appendChild(revokeText);
+      a.setAttribute("href", "#");
+      a.setAttribute("id", "revoke-cookies");
+      a.addEventListener("click", acceptOrRevokeCookies);
+      p.appendChild(a);
+      cookies.append(p);
+    } else {
+      e.target.parentElement.remove();
+      const a = document.createElement("a");
+      const cookiesText = document.createTextNode("Accept cookies");
+      a.appendChild(cookiesText);
+      a.setAttribute("href", "#");
+      a.setAttribute("id", "accept-cookies");
+      a.addEventListener("click", acceptOrRevokeCookies);
+      cookies.appendChild(a);
+    }
   }
 }
 
@@ -106,4 +110,8 @@ function changeContrast() {
 
 // Part 4: Keyboard Shortcut
 
-
+document.addEventListener("keydown", function (e) {
+  if (e.ctrlKey && e.key === "a") {
+    changeContrast();
+  }
+});
